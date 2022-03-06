6c408e5f439dd1e88c35879fdbc3cbb4502bb1cb
Nguyen The Nam <namnguyen61031@gmail.com>
2022-03-06 14:52:41 +0700

    Put default configs inside a function with return statement

--- metric_learning/default_configs.py
+++ metric_learning/default_configs.py
@@ -3,34 +3,37 @@ import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 from types import SimpleNamespace
 
-cfg = get_default_configs()
+def get_configs():
+    cfg = get_default_configs()
 
-cfg.tensor_inp_shape = (512, 512, 3)
+    cfg.tensor_inp_shape = (512, 512, 3)
 
-cfg.global_pool = "gem"
-cfg.n_classes = 1000
-cfg.embedding_size = 512
+    cfg.global_pool = "gem"
+    cfg.n_classes = 1000
+    cfg.embedding_size = 512
 
-# arcface
-cfg.margin = 45
-cfg.scale = 0.3
-cfg.adaptive_margin = False
-cfg.sub_center = False
-cfg.arcface_m_x = 0.45
-cfg.arcface_m_y = 0.05
+    # arcface
+    cfg.margin = 45
+    cfg.scale = 0.3
+    cfg.adaptive_margin = False
+    cfg.sub_center = False
+    cfg.arcface_m_x = 0.45
+    cfg.arcface_m_y = 0.05
 
-# dolg
-cfg.dolg_dilations = [6,12,18]
+    # dolg
+    cfg.dolg_dilations = [6,12,18]
 
-# augmentations
-cfg.train_trans = A.Compose([
-        A.Resize(cfg.tensor_inp_shape[0], cfg.tensor_inp_shape[1]),
-        A.Normalize(),
-        ToTensorV2(p=1.0),
-    ])
+    # augmentations
+    cfg.train_trans = A.Compose([
+            A.Resize(cfg.tensor_inp_shape[0], cfg.tensor_inp_shape[1]),
+            A.Normalize(),
+            ToTensorV2(p=1.0),
+        ])
 
-cfg.valid_trans = A.Compose([
-        A.Resize(cfg.tensor_inp_shape[0], cfg.tensor_inp_shape[1]),
-        A.Normalize(),
-        ToTensorV2(p=1.0),
-    ])
+    cfg.valid_trans = A.Compose([
+            A.Resize(cfg.tensor_inp_shape[0], cfg.tensor_inp_shape[1]),
+            A.Normalize(),
+            ToTensorV2(p=1.0),
+        ])
+
+    return cfg
