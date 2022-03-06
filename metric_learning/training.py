ee053dd790131ce990ecb7917f2ef3749105eea0
Nguyen The Nam <namnguyen61031@gmail.com>
2022-03-06 14:18:39 +0700

    Add some functions in training.py

--- metric_learning/training.py
+++ metric_learning/training.py
@@ -55,3 +55,43 @@ def train_fn(dataloader,model,optimizer,scaler,device,scheduler,epoch):
         scheduler.step()
         
     return loss_score.avg
+
+def get_all_embeddings(dataloader, model, device='cuda:0'):
+    model.eval()
+    tk0 = tqdm(enumerate(dataloader), total=len(dataloader))
+    all_embeddings = []
+    for i,batch in tk0:
+        
+        for k in batch:
+            batch[k] = batch[k].to(device)
+        batch_size = len(batch['input'])
+        
+        with torch.no_grad():
+            out = model(batch)
+        loss, target, conf, cls, embeddings = out['loss'], out['target'], out['preds_conf'], out['preds_cls'], out['embeddings']
+        all_embeddings.append(embeddings.cpu().numpy())
+    return np.concatenate(all_embeddings)
+
+def get_all_embeddings_and_predictions(dataloader, model, device='cuda:0'):
+    model.eval()
+    tk0 = tqdm(enumerate(dataloader), total=len(dataloader))
+    all_embeddings = []
+    all_pred_classes = []
+    all_pred_confs = []
+    for i,batch in tk0:
+        
+        for k in batch:
+            batch[k] = batch[k].to(device)
+        batch_size = len(batch['input'])
+        
+        with torch.no_grad():
+            out = model(batch)
+        loss, target, conf, cls, embeddings = out['loss'], out['target'], out['preds_conf'], out['preds_cls'], out['embeddings']
+        all_embeddings.append(embeddings.cpu().numpy())
+        all_pred_classes.append(cls.cpu().numpy())
+        all_pred_confs.append(conf.cpu().numpy())
+
+    out_dict = {'embeddings': np.concatenate(all_embeddings),
+                'pred_class':np.concatenate(all_pred_classes), 
+                'pred_conf': np.concatenate(all_pred_confs)}
+    return out_dict
